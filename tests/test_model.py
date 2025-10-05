0c0bb2dd4ca07fef4e893cc480ccc46a5ea34afc
Yashvardhan Thanvi <yashvt9404@gmail.com>
2025-10-05 20:43:48 +0530

    Add model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ import pytest
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from model import ExoplanetModel
+from src.model import ExoplanetModel
 
 
 def test_model_initialization():
